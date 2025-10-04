8f7aecbb4b6d3bb210e9832a5040269ef2bba2bb
Ngurah Dewa <ngurahdewa17@gmail.com>
2025-10-04 12:54:12 +0800

    Autentikasi Login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
